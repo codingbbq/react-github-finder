84d5d558f8d19b8d0ea0e787e1ba928df271aa03
codingbbq <codingbbq@gmail.com>
2023-08-29 13:52:47 +0530

    Website url fix

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -105,7 +105,7 @@ function User() {
               <div className='stat'>
                 <div className='stat-title text-md'>Website</div>
                 <div className='text-lg stat-value'>
-                  <a href={`https://${blog}`} target='_blank' rel='noreferrer'>
+                  <a href={blog} target='_blank' rel='noreferrer'>
                     {blog}
                   </a>
                 </div>
